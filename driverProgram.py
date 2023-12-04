93268b08424ed89a815ebcb23638882003540556
vionadjunaidy <144786402+vionadjunaidy@users.noreply.github.com>
2023-12-04 16:15:29 +0700

    Update driverProgram.py

--- driverProgram.py
+++ driverProgram.py
@@ -4,20 +4,36 @@ def list_of_objectsVD():
     objects = []
 
     while True:
-        no_of_items = input(f'Input the number of items: ')
+        no_of_items = int(input(f'How many items will your order today? '))
     
         if no_of_items > 0:
             break
+        else:
+            print(f'Number of items must be at least 1.')
         
     for item in range(no_of_items):
-        name_of_item = input(f'Input name of item: ')
+        print(f'Item #{item+1}')
+        name_of_item = input(f'Enter food: ')
 
         while True:
-            no_of_pounds = input(f'Input the number of pounds: ')
+            no_of_pounds = float(input(f'Enter amount of pounds: '))
             if no_of_pounds > 0:
                 break
         
-        objects.append(Food(name_of_item, no_of_pounds))
-   
+        patungVio = Food(name_of_item, no_of_pounds)
+        objects.append(patungVio)
 
-list_of_objectsVD()
+    return objects
+
+def contents_of_listVD(objects):
+    for item in objects:
+        print(f'Name: {item.getName()}')
+        print(f'Amount: {item.getAmount()}')
+        print(f'Price: {item.getPrice()}')
+        print(f'Total Cost: {item.calculateCostVD()}')
+
+def main():
+    vio = list_of_objectsVD()
+    contents_of_listVD(vio)
+
+main()
